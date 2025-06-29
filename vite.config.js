001a0c5504f13b114557bee56d49557864f32489
123 Notus <45607902+123-Notus@users.noreply.github.com>
2025-06-29 11:52:40 +0000

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/role-preview-for-discord/",
+  base: "/role-preview/",
   plugins: [react()],
   resolve: {
     alias: {
